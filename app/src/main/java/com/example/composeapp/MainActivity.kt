85ccc0e8d9399b8b0d8f3709944675d13b406403
Shaparapatah <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>
2022-06-23 11:43:38 +0300

    1. Сделал прикольную анимацию с выскакивающей надписью из под картинки

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,43 +1,45 @@
 package com.example.composeapp
 
+
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+
+
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            JetPackCompose()
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeAppTheme {
-        Greeting("Android")
+ fun JetPackCompose() {
+    Card {
+         var expended by remember { mutableStateOf(false) }
+        Column(Modifier.clickable { expended = !expended }) {
+            Image(painterResource(R.drawable.photo), "11")
+            AnimatedVisibility(expended) {
+                Text(
+                    text = "It's me :)",
+                    style = MaterialTheme.typography.h2,
+                )
+            }
+        }
     }
 }
